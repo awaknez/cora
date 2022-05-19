e461b7d73160d87b85f149cdea43e28255d0f7e2
awaknez <zenz022021@gmail.com>
2022-05-19 20:27:36 +0900

    createアクション実装

--- app/controllers/reservations_controller.rb
+++ app/controllers/reservations_controller.rb
@@ -10,19 +10,22 @@ class ReservationsController < ApplicationController
     #  binding.pry
      @date = params[:date]
      @time = params[:time]
+     @start_time = DateTime.parse(@date + " " + @time + " " +"JST")
    end
 
-  # def create
-  #   @reservation = Reservation.new(reservation_params)
-  #   if @reservation.save
-  #     redirect_to reservation_path @reservation.id
-  #   else
-  #     render :new
-  #   end
-  # end
+  def create
+    @reservation = Reservation.new(reservation_params)
+    if @reservation.save
+      redirect_to root_path
+      # reservation_path(@reservation.id)
+    else
+      render :new
+    end
+  end
 
-  # private
-  # def reservation_params
-  #   params.require(:reservation).permit([:date,:time,:start_time.:style_id,:number_of_people_id,:question]).merge(user_id:current_user.id)
-  # end
+  private
+  def reservation_params
+    params.require(:reservation).permit(:date,:time,:start_time,:style_id,:number_of_people_id,:question).merge(user_id:current_user.id)
+  end
+
 end
