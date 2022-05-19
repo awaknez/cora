90e3198853e421fa71b30db507e2f045a9ddfdfd
awaknez <zenz022021@gmail.com>
2022-05-19 16:19:34 +0900

    カレンダー予約可能日表示の実装

--- app/controllers/reservations_controller.rb
+++ app/controllers/reservations_controller.rb
@@ -2,4 +2,22 @@ class ReservationsController < ApplicationController
   def index
     @reservations = Reservation.all
   end
+
+  # def new
+  #   @reservation = Reservation.new
+  # end
+
+  # def create
+  #   @reservation = Reservation.new(reservation_params)
+  #   if @reservation.save
+  #     redirect_to reservation_path @reservation.id
+  #   else
+  #     render :new
+  #   end
+  # end
+
+  # private
+  # def reservation_params
+  #   params.require(:reservation).permit([:date,:time,:start_time.:style_id,:number_of_people_id,:question]).merge(user_id:current_user.id)
+  # end
 end
--- app/models/reservation.rb
+++ app/models/reservation.rb
@@ -1,15 +1,16 @@
 class Reservation < ApplicationRecord
-  # def self.reservations_after_three_month
-  #   # 今日から3ヶ月先までのデータを取得
-  #   reservations = Reservation.all.where("day >= ?", Date.current).where("day < ?", Date.current >> 3).order(date: :desc)
-  #   # 配列を作成し、データを格納
-  #   # DBアクセスを減らすために必要なデータを配列にデータを突っ込んでます
-  #   # reservation_data = []
-  #   # reservations.each do |reservation|
-  #   #   reservations_hash = {}
-  #   #   reservations_hash.merge!(date: reservation.date.strftime("%Y-%m-%d"), time: reservation.time)
-  #   #   reservation_data.push(reservations_hash)
-  #   # end
-  #   # reservation_data
-  #  end
+  def self.reservations_after_three_month
+    # 今日から3ヶ月先までのデータを取得
+    # 下記の”date>=?”のdateはカラム名
+    reservations = Reservation.all.where("date >= ?", Date.current).where("date < ?", Date.current >> 3).order(date: :desc)
+    # 配列を作成し、データを格納
+    # DBアクセスを減らすために必要なデータを配列にデータを突っ込んでます
+    reservation_data = []
+    reservations.each do |reservation|
+      reservations_hash = {}
+      reservations_hash.merge!(day: reservation.date.strftime("%Y-%m-%d"), time: reservation.time)
+      reservation_data.push(reservations_hash)
+    end
+    reservation_data
+   end
 end
