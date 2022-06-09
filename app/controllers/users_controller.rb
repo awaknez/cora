c5c224bf6ca279911558ad91799dc4e4ee4597f0
awaknez <zenz022021@gmail.com>
2022-06-10 08:38:21 +0900

    他ユーザーのマイページへ遷移できないように変更

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,8 +1,12 @@
 class UsersController < ApplicationController
   def show
-    @user = User.find(params[:id])
-    @user_name = "#{(@user.name_last_name)+(@user.name_first_name)}"
-    @reservations = @user.reservations.order(date: "ASC" ,time: "ASC")
+    if params[:id].to_i != current_user.id
+      redirect_to root_path
+    else
+      @user = User.find(params[:id])
+      @user_name = "#{(@user.name_last_name)+(@user.name_first_name)}"
+      @reservations = @user.reservations.order(date: "ASC" ,time: "ASC")
+    end
   end
 
 end
